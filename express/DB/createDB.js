4b9d1b876a4d24cdef4347aecc3e01d44964c23b
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-22 12:16:44 +0700

    fix error sql

--- express/DB/createDB.js
+++ express/DB/createDB.js
@@ -1,7 +1,7 @@
-const db = require('./configDB')
-const sql = 'CREATE DATABASE FID'
+const db = require("./configDB");
+const sql = "CREATE DATABASE biodata";
 
-db.query(sql, err=>{
-    if(err) throw err
-    console.log("DB Berhasil Dibuat")
-})
+db.query(sql, (err) => {
+  if (err) throw err;
+  console.log("DB Berhasil Dibuat");
+});
--- express/DB/createTABLE.js
+++ express/DB/createTABLE.js
@@ -1,17 +1,16 @@
-const db = require('./configDB')
+const db = require("./configDB");
 
-//nama table adalah users
-const sql = `CREATE TABLE ikannn
+//nama table adalah user
+const sql = `CREATE TABLE user
             (
                 id INT NOT NULL AUTO_INCREMENT,
-                name VARCHAR(30),
-                tgl_lhr VARCHAR(11),
-                bio VARCHAR(255),
+                nama VARCHAR(30),
+                umur VARCHAR(11),
+                hobi VARCHAR(255),
                 PRIMARY KEY (id)
             )
-            `
+            `;
 
-db.query(sql, (err, result)=> {
-    if(err) throw err 
-    console.log("Table Berhasil Dibuat")
-})
+db.query(sql, (e) => {
+  e ? console.error(e) : console.log("Table Berhasil Dibuat");
+});
